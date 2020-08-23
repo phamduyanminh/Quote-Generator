2bebe5ba02513527409889beb99f07e1e2982a4d
Minh Pham <phamduyanminh@users.noreply.github.com>
2020-08-23 18:25:05 -0400

    Finished fetching api to display quotes on website

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-//Get Quote From API
-async function getQuote(){
+const apiWebsite = "https://api.quotable.io/random";
 
-}
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const btn = document.getElementById("new-quote");
 
+btn.addEventListener("click", getQuote);
 
-//On Load
+function getQuote() {
+   fetch(apiWebsite)
+     .then((res) => res.json())
+     .then((data) => {
+       quote.innerHTML = `"${data.content}"`;
+       author.innerHTML = `- ${data.author}`;
+     });
+ }
